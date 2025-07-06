aa2940c8b534a4358fd82ac74b85735d78a67224
Kayboy15 <takundaamin074@gmail.com>
2025-07-06 16:08:07 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,15 @@
+const settings = {
+  packname: 'KEITH XMD',
+  author: '‎',
+  botName: "Kᴇɪᴛʜ-Xᴍᴅ",
+  botOwner: 'Kᴇɪᴛʜ', // Your name
+  ownerNumber: '263789745277', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.2.0",
+  menuImageUrl: "https://files.catbox.moe/x36yev.png",
+  menuAudioUrl: ""
+};
+
+module.exports = settings;
